f421269e91d09608aaddef9d915802906853a149
mirage <mirage.kamran.pkt@gmail.com>
2020-04-28 19:33:13 -0700

    Syles added to the page

--- app/src/components/CuteDogs.js
+++ app/src/components/CuteDogs.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getDog } from "../actions";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 
 const CuteDogs = ({ getDog, isFetching, dogImg, error }) => {
   if (isFetching) {
     return <h2>Fetching a Dog Image now :) </h2>;
   } else {
     return (
-      <div>
-        <img width="200" src={dogImg} />
-        <button onClick={getDog}>Get a Dog Image</button>
-      </div>
+      <Card >
+        <CardActionArea >
+          <CardMedia >
+            <img style={{borderRadius: '3px'}} width="500" src={dogImg} />
+          </CardMedia>
+        </CardActionArea>
+        <Button variant="contained" color="secondary" onClick={getDog}>Get a Dog Image</Button>
+      </Card>
     );
   }
 };
