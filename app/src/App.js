b2d313dd9d9572264744e1a4930422a0540b798c
mirage <mirage.kamran.pkt@gmail.com>
2020-04-28 22:20:49 -0700

    some colors added to the page

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import CuteDogs from './components/CuteDogs';
-import './App.css';
+import React from "react";
+import CuteDogs from "./components/CuteDogs";
+import "./App.css";
 import Card from "@material-ui/core/Card";
 
 function App() {
   return (
     <div className="App">
-      <Card>
+      <Card style={{backgroundColor: '#43e0d9'}}>
         <h1>Dogs World</h1>
       </Card>
       <CuteDogs />
--- app/src/components/CuteDogs.js
+++ app/src/components/CuteDogs.js
@@ -10,16 +10,16 @@ import CardMedia from "@material-ui/core/CardMedia";
 
 const CuteDogs = ({ getDog, isFetching, dogImg, error }) => {
   if (isFetching) {
-    return <h2>Fetching a Dog Image now :) </h2>;
+    return <h2>Fetching a Dog Image now 🐕 </h2>;
   } else {
     return (
-      <Card >
-        <CardActionArea >
-          <CardMedia >
-            <img style={{borderRadius: '3px'}} width="500" src={dogImg} />
-          </CardMedia>
-        </CardActionArea>
-        <Button variant="contained" color="secondary" onClick={getDog}>Get a Dog Image</Button>
+      <Card style={{ backgroundColor: "#409cff" }}>
+        <CardMedia>
+          <img style={{ borderRadius: "10px" }} width="500" src={dogImg} />
+        </CardMedia>
+        <Button variant="contained" color="secondary" onClick={getDog}>
+          Get a Dog Image
+        </Button>
       </Card>
     );
   }
